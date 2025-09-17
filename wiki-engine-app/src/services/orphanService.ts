d5e0fe31db669bf40b52b3c0a0e0bc2bdc771b80
keizie <github@keizie.net>
2025-09-18 07:42:55 +0900

    feat: orphaned file(고아 파일) 관리 서비스 스텁 추가 (orphanService)

---
+++ wiki-engine-app/src/services/orphanService.ts
@@ -0,0 +1,10 @@
+// 고아 파일(Orphaned File) 관리 서비스 (스텁)
+
+export function findOrphanedFiles(): string[] {
+  // TODO: WikiPage와 연결되지 않은 파일 목록 반환
+  return [];
+}
+
+export function deleteOrphanedFile(_filePath: string): void {
+  // TODO: 고아 파일 삭제 로직 구현
+}
