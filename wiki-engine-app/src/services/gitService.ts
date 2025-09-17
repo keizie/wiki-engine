fdbae819b18245f77e98309e891b58245d613caf
keizie <github@keizie.net>
2025-09-18 07:38:48 +0900

    Add stub for Git commit and squash service (T007, linted)

---
+++ wiki-engine-app/src/services/gitService.ts
@@ -0,0 +1,9 @@
+// Git 연동 및 커밋 관리 서비스 (스텁)
+
+export function commitChange(_message: string): void {
+  // TODO: 파일/페이지 변경 시 자동 커밋 로직 구현
+}
+
+export function squashCommits(): void {
+  // TODO: 커밋 스쿼시 기능 구현
+}
